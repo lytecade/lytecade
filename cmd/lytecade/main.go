6111dc22104202b5feb0244932ff18ce325b5992
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-11-25 17:33:01 -0500

    feat: add basic structure + handler class

--- cmd/lytecade/main.go
+++ cmd/lytecade/main.go
@@ -3,18 +3,12 @@ package main
 import (
     "fmt"
     "net/http"
+	"github.com/lytecade/lytecade/internal/handlers"
 )
 
-// Handler function to serve HTTP requests
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "LYTECADE")
-}
 
 func main() {
-    // Set up route and handler
-    http.HandleFunc("/", handler)
-
-    // Start the web server
+    http.HandleFunc("/", handlers.RunIndexHandler)
     fmt.Println("Starting server on :8080...")
     err := http.ListenAndServe(":8080", nil)
     if err != nil {
---
+++ internal/handlers/handlers.go
@@ -0,0 +1,10 @@
+package handlers
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func RunIndexHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "LYTECADE-HANDLER")
+}
