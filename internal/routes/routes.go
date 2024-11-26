8d5a41e04628090014dd83239faf26eeba1209f9
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-11-26 16:06:09 -0500

    feat: add favicon and static file server

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -7,6 +7,8 @@ import (
 )
 
 func RoutesInit() {
+	fs := http.FileServer(http.Dir("./web/static"))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
 	http.HandleFunc("/", pages.HandleHomePage)
 	http.HandleFunc("/about", pages.HandleAboutPage)
 	err := http.ListenAndServe(":8080", nil)
