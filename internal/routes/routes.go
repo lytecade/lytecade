5acb584c4f2b21cb69bb3468abcb174be4b3d8d8
dylanxhernandez <dylan.x.h.developer@gmail.com>
2024-12-23 11:59:38 -0500

    refactor: cleanup spacing on internal files

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,12 +1,12 @@
 package routes
 
 import (
-	"fmt"
-	"os"
-	"strings"
-	"net/http"
-	"github.com/lytecade/lytecade/internal/pages"
-	"path/filepath"
+    "fmt"
+    "os"
+    "strings"
+    "net/http"
+    "github.com/lytecade/lytecade/internal/pages"
+    "path/filepath"
 )
 
 var (
@@ -14,59 +14,59 @@ var (
 )
 
 func RouteInit() {
-	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./web/static"))))
-	http.HandleFunc("/",  func(w http.ResponseWriter, r *http.Request) {
-		pages.HandleHomePage(w, r, GetRouteSites())
-	})
-	http.HandleFunc("/about", pages.HandleAboutPage)
+    http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./web/static"))))
+    http.HandleFunc("/",  func(w http.ResponseWriter, r *http.Request) {
+        pages.HandleHomePage(w, r, GetRouteSites())
+    })
+    http.HandleFunc("/about", pages.HandleAboutPage)
 }
 
 func RouteSites() {
-	err := filepath.Walk("./web/sites", func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() {
-			return nil
-		}
-		fmt.Println(path)
-		currentSite := trimMultiplePrefixes(path, []string{"./web/sites","web/sites"})
-		if currentSite == "" {
-			return nil
-		}
+    err := filepath.Walk("./web/sites", func(path string, info os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
+        if !info.IsDir() {
+            return nil
+        }
+        fmt.Println(path)
+        currentSite := trimMultiplePrefixes(path, []string{"./web/sites","web/sites"})
+        if currentSite == "" {
+            return nil
+        }
         if !contains(currentSites, currentSite) {
             currentSites = append(currentSites, currentSite)
         }
-		fmt.Println(fmt.Sprintf("%s", currentSite))
-		http.HandleFunc(fmt.Sprintf("%s", currentSite), func(w http.ResponseWriter, r *http.Request) {
-			pages.HandleGamePage(w, r, currentSite)
-		})
-		http.Handle(fmt.Sprintf("%s/assets/", currentSite), pages.HandleGameFolder(currentSite, path, "assets"))
-		http.Handle(fmt.Sprintf("%s/src/", currentSite), pages.HandleGameFolder(currentSite, path, "src"))
+        fmt.Println(fmt.Sprintf("%s", currentSite))
+        http.HandleFunc(fmt.Sprintf("%s", currentSite), func(w http.ResponseWriter, r *http.Request) {
+            pages.HandleGamePage(w, r, currentSite)
+        })
+        http.Handle(fmt.Sprintf("%s/assets/", currentSite), pages.HandleGameFolder(currentSite, path, "assets"))
+        http.Handle(fmt.Sprintf("%s/src/", currentSite), pages.HandleGameFolder(currentSite, path, "src"))
 
-		return nil
-	})
-	if err != nil {
-		fmt.Println("Error registering game server:", err)
-	}
+        return nil
+    })
+    if err != nil {
+        fmt.Println("Error registering game server:", err)
+    }
 }
 
 func RouteListen() {
-	fmt.Println("Listening on Port 8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println("Error starting server:", err)
-	}
+    fmt.Println("Listening on Port 8080")
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        fmt.Println("Error starting server:", err)
+    }
 }
 
 func GetRouteSites() []string {
-	var siteNameArray []string
-	for _, path := range currentSites {
-		parts := strings.Split(path, "/")
-		if !contains(siteNameArray, parts[1]) {
-			siteNameArray = append(siteNameArray, parts[1])
-		}
-	}
+    var siteNameArray []string
+    for _, path := range currentSites {
+        parts := strings.Split(path, "/")
+        if !contains(siteNameArray, parts[1]) {
+            siteNameArray = append(siteNameArray, parts[1])
+        }
+    }
     return siteNameArray
 }
 
